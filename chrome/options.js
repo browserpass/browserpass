ce8e255f114474b6cac8caf6938da99447b03583
Maxim Baz <git@maximbaz.com>
2018-03-24 22:09:04 +0100

    Add selector for custom password stores to options

--- chrome/options.js
+++ chrome/options.js
@@ -1,23 +1,44 @@
 function save_options() {
-  var autoSubmit = document.getElementById("auto-submit").checked;
+  let autoSubmit = document.getElementById("auto-submit").checked;
   localStorage.setItem("autoSubmit", autoSubmit);
 
   // Options related to fuzzy finding.
   //  use_fuzzy_search indicates if fuzzy finding or glob searching should
   //  be used in manual searches
-  var use_fuzzy = document.getElementById("use-fuzzy").checked;
+  let use_fuzzy = document.getElementById("use-fuzzy").checked;
   localStorage.setItem("use_fuzzy_search", use_fuzzy);
+
+  let groups = document.getElementsByClassName("password-store-group");
+  let paths = [];
+  for (let i = 0; i < groups.length; i++) {
+    let group = groups[i];
+    let enabled = group.querySelector(".password-store-path-enabled").checked;
+    let path = group.querySelector(".password-store-path").value;
+    if (path) {
+      paths.push({ path: path, enabled: enabled });
+    }
+  }
+  localStorage.setItem("paths", JSON.stringify(paths));
 
   window.close();
 }
 
 function restore_options() {
-  var autoSubmit = localStorage.getItem("autoSubmit") == "true";
+  let autoSubmit = localStorage.getItem("autoSubmit") == "true";
   document.getElementById("auto-submit").checked = autoSubmit;
 
   // Restore the view to show the settings described above
-  var use_fuzzy = localStorage.getItem("use_fuzzy_search") != "false";
+  let use_fuzzy = localStorage.getItem("use_fuzzy_search") != "false";
   document.getElementById("use-fuzzy").checked = use_fuzzy;
+
+  let groups = document.getElementsByClassName("password-store-group");
+  let paths = JSON.parse(localStorage.getItem("paths") || "[]");
+  for (let i = 0; i < paths.length; i++) {
+    let path = paths[i];
+    let group = groups[i];
+    group.querySelector(".password-store-path-enabled").checked = path.enabled;
+    group.querySelector(".password-store-path").value = path.path;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", restore_options);
