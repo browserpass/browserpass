2dea7c0c5d84cbea1d8ba36ec0699b079952e1bc
Maxim Baz <git@maximbaz.com>
2018-03-27 11:01:36 +0200

    Resolve conflict with master

--- cmd/browserpass/main.go
+++ cmd/browserpass/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -9,9 +11,18 @@ import (
 	"github.com/dannyvankooten/browserpass/protector"
 )
 
+const VERSION = "2.0.17"
+
 func main() {
 	protector.Protect("stdio rpath proc exec getpw")
 	log.SetPrefix("[Browserpass] ")
+
+	showVersion := flag.Bool("v", false, "print version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Println("Browserpass host app version:", VERSION)
+		os.Exit(0)
+	}
 
 	if err := browserpass.Run(os.Stdin, os.Stdout); err != nil && err != io.EOF {
 		log.Fatal(err)
